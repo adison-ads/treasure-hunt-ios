35a37fb86c1191764d25ac7011d4d39ae84c39d5
kimjaehyeon-nbt <kim.jaehyeon@nbt.com>
2023-06-30 18:54:03 +0900

    release 0.19.2

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "TreasureHunt",
-            url: "https://github.com/adison-ads/treasure-hunt-ios/releases/download/v0.19.0/TreasureHuntWebtoon.zip",
-            checksum: "29cf338a9a7575d965056a596410063a0fd2381dc9efcdaba9483fa5c731c2d3")
+            url: "https://github.com/adison-ads/treasure-hunt-ios/releases/download/v0.19.2/TreasureHuntWebtoon.zip",
+            checksum: "5a533262df95ee009dd39809930a0aabe139b6e90819ad0c9ca24cfaa12b790e")
     ]
 )
