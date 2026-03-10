d95481d93986392ddce673575adbdb5f7f8d2f1d
Bitrise CI <noreply@bitrise.io>
2026-03-10 10:28:31 +0000

    1.1.0 릴리즈

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@
 
 import PackageDescription
 
-let version = "1.0.4"
-let checksum = ""
-let baseVersion = "1.1.0"
+let version = "1.1.0"
+let checksum = "5fd9111aced088ad7a0e2ad434f59a68b13477bff0602ce20821743399e24f77"
+let baseVersion = "1.2.0"
 let nativeAdVersion = "1.0.0"
 
 let package = Package(
@@ -21,7 +21,7 @@ let package = Package(
             exact: Version(stringLiteral: baseVersion)),
         .package(
             url: "https://github.com/adison-ads/adison-offerwall-global-native-ad",
-            exact: version(stringLiteral: nativeAdVersion)),
+            exact: Version(stringLiteral: nativeAdVersion)),
     ],
     targets: [
         .binaryTarget(
